cbadbc9a9a177c9a4fee553ae798b716c8cacc5f
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-29 12:46:02 +0530

    if else_if else Javascript Programing Constructs

---
+++ if_elseif_else/single_digit_into_word.js
@@ -0,0 +1,29 @@
+// Importing the module
+
+const readline = require("prompt-sync");
+const prompt=readline();
+
+// Enter the number
+
+let a = prompt("Enter Number : ");
+
+if(a==1)
+console.log("One");
+else if(a==2)
+console.log("Two");
+else if(a==3)
+console.log("Three");
+else if(a==4)
+console.log("four");
+else if(a==5)
+console.log("Five");
+else if(a==6)
+System.out.println("Six");
+else if(a==7)
+console.log("Seven");
+else if(a==8)
+console.log("Eight");
+else if(a==9)
+console.log("Nine");
+else
+console.log("invalid Digit");
